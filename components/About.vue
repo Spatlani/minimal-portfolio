dd41133297732a89b7fea025afe1dc8000fd33b6
Shubham Patlani <Spatlani@gmail.com>
2022-04-12 16:23:40 +0530

    Marketing 101 added

--- components/About.vue
+++ components/About.vue
@@ -2,21 +2,21 @@
   <v-row align="center" justify="center">
     <v-col cols="12" md="8">
       <v-row>
-        <v-col cols="12" class="text-h3 font-weight-medium">
-          Hi, I am Ruchi Raj
+        <v-col cols="12" class="text-h6 font-weight-medium">
+          Hi, I am Ruchi Raj. <br> Implementation specialist and customer success manager.
         </v-col>
-        <v-col cols="12" class="text-h6 font-weight-bold">
-          Implementation Specialist and Customer Success Manager
-        </v-col>
-        <v-col cols="12" class="title text--secondary py-0">
-          I believe in Fast | Fail | Forward
+        <v-col cols="12" class="text-h3 font-weight-bold">
+          Transparent communication disciplines the chaos.
         </v-col>
         <v-col cols="12" class="text-justify body-2">
-          Transparent communication disciplines the chaos. This belief keeps me on my toes and remains a constant source of motivation throughout my role as a customer success manager.
+          This belief keeps me on my toes and remains a constant source of motivation throughout my role as a customer success manager.
           I have 5 + years of experience in customer success, including customer on-boarding, training, and implementation of the SaaS software onsite in 10+ countries including Thailand, the Philippines, Vietnam, Bangladesh, India, and offsite in Australia, the US, New Zealand, and Europe.
         </v-col>
         <v-col cols="12" class="text-justify body-2">
           Reducing churn and maintaining the analysis of customer usage for scaling up has been a crucial part of my role. Problem-solving was always my hobby and it gradually shaped my career steer towards this side of the business. I love helping people and believe that it’s better to empower than to sympathize, so I offer sessions on image enhancement in my free hours to NGOs and other social organizations.
+        </v-col>
+        <v-col cols="12" class="subtitle-1 text--secondary py-0">
+          I believe in Fast | Fail | Forward
         </v-col>
         <v-col cols="12" class="body-2">
           <v-btn color="black white--text" elevation="0" class="text-capitalize mr-2" :href="mailingAddress">
